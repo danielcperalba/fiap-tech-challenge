b927589d706cadce32b49ac54c552b57fe8ac887
gabrielluiz98 <gabrielluiz3011@gmail.com>
2025-05-07 22:47:55 -0300

    Adição do método de autenticação

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from fastapi import Depends, HTTPException, status
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
 
 app = FastAPI(
     title="Tech Challenge",
@@ -6,6 +8,44 @@ app = FastAPI(
     description="Projeto do Tech Challenge Fiap"
 )
 
+# Banco de dados de usuários em memória
+users = {
+    "admin": "admin"
+}
+
+security = HTTPBasic()
+
+def verify_password(credentials: HTTPBasicCredentials = Depends(security)):
+    username = credentials.username
+    password = credentials.password
+    if username in users and username[username] == password:
+        return username
+    raise HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail='Credenciais inválidas',
+        headers={'WWW-Authenticate': "Basic"}
+    )
+
 @app.get("/")
-async def home():
+async def home(username: str = Depends(verify_password)):
     return "Hello World"
+
+@app.get("/producao")
+async def home(username: str = Depends(verify_password)):
+    return "Produção"
+
+@app.get("/processamento")
+async def home(username: str = Depends(verify_password)):
+    return "Processamento"
+
+@app.get("/comericalizacao")
+async def home(username: str = Depends(verify_password)):
+    return "Comercialização"
+
+@app.get("/importacao")
+async def home(username: str = Depends(verify_password)):
+    return "Hello World"
+
+@app.get("/exportacao")
+async def home(username: str = Depends(verify_password)):
+    return "Exportação"
